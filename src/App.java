c86686b7a18d87f9d788e697b4561875ee02df61
CarlosMatheus2005 <carlos.eufrazio1@gmail.com>
2026-05-19 10:00:48 -0300

    commit_simplex

---
+++ src/App.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Informe o Número de Variáveis: ");
+        int var = sc.nextInt();   
+
+        System.out.println("Informe o Número de Restrições: ");
+        int rest = sc.nextInt();
+
+        
+
+    }
+}
