8a47a7de6a2c62e1239679dc48ffb694b44469fa
CarlosMatheus2005 <carlos.eufrazio1@gmail.com>
2026-05-19 10:07:55 -0300

    atualizacao

--- src/App.java
+++ src/App.java
@@ -10,7 +10,9 @@ public class App {
         System.out.println("Informe o Número de Restrições: ");
         int rest = sc.nextInt();
 
-        
+        double[][] A = new double[rest][var];
+        double[] b = new double[var];
+        double[] c = new double[var];
 
     }
 }
