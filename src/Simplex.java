6a39d9a3425f6711b22a73a36624911d476ff1a2
CarlosMatheus2005 <carlos.eufrazio1@gmail.com>
2026-05-19 20:44:37 -0300

    atualiazao

--- src/Simplex.java
+++ src/Simplex.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
 public class Simplex {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         //Variáveis para -> x1, x2, ..., xn
         System.out.print("Informe o Número de Variáveis: ");
         int n = sc.nextInt();
-        
+
         //Quantidade de Restricoes <=
         System.out.print("Informe o Número de Restrições: ");
         int m = sc.nextInt();
@@ -22,77 +22,63 @@ public class Simplex {
         //Funcao Objetivo
         double[] z = new double[n];
 
-        //Variaveis de Folga 
-        double[][] f = new double[m][m];
-                
-        //Variaveis de Folga que depois virarao as variaveis normais 
+        //Variaveis de Folga que depois virarao as variaveis normais
         String[] var = new String[m];
 
         //Tabela Tableau (Linhas: restrições + Função Objetivo, Colunas: variaveis + restrições + FO)
-        double[][] tableau = new double[m+1][n+m+1];
+        double[][] tableau = new double[m + 1][n + m + 1];
 
         //Montando a Função Objetivo (z)
-        for(int i = 0; i < n; i++){
-            System.out.print("Informe o coeficiente x"+(i+1)+": ");
+        for (int i = 0; i < n; i++) {
+            System.out.print("Informe o coeficiente x" + (i + 1) + ": ");
             z[i] = sc.nextDouble();
         }
 
         System.out.print("Função objetivo: z = ");
-        for(int i = 0; i < n; i++){
-            if(i == n-1){
-                System.out.print("("+z[i]+")x"+(i+1));
-            }else{
-                System.out.print("("+z[i]+")x"+(i+1)+" + ");    
-            } 
+        for (int i = 0; i < n; i++) {
+            System.out.print("(" + z[i] + ")x" + (i + 1));
+            System.out.print(i == n - 1 ? "" : " + ");
         }
         System.out.println();
 
         //Montando as Restrições
-        for(int i = 0; i < m; i++){
-            System.out.println("\nRestrição "+(i+1)); 
+        for (int i = 0; i < m; i++) {
+            System.out.println("\nRestrição " + (i + 1));
 
-            for(int j = 0; j < n; j++){
-                System.out.print("Informe o coeficiente x"+(j+1)+": ");
+            for (int j = 0; j < n; j++) {
+                System.out.print("Informe o coeficiente x" + (j + 1) + ": ");
                 a[i][j] = sc.nextDouble();
             }
 
             System.out.print("Resultado (b): ");
             b[i] = sc.nextDouble();
-            f[i][i] = 1;
-            var[i] = "f"+(i+1);
+
+            var[i] = "f" + (i + 1);
         }
         System.out.println();
 
-        //Montando o Tableu {
-            //Preenchendo a tabela com as restrições
-            for(int i = 0; i < m; i++){
-                //Coeficientes das variáveis normais
-                for(int j = 0; j < n; j++){
-                    tableau[i][j] = a[i][j];
-                }
-                //Variáveis de folga
-                    for(int j = 0; j < m; j++){
-                    tableau[i][n + j] = f[i][j];
-                }
-                //Coluna b
-                tableau[i][n + m] = b[i];
+        //Montando o Tableu
+        //Preenchendo a tabela com as restrições
+        for (int i = 0; i < m; i++) {
+
+            //Coeficientes das variáveis normais
+            for (int j = 0; j < n; j++) {
+                tableau[i][j] = a[i][j];
             }
 
-            //Função objetivo
-            for(int j = 0; j < n; j++){
-                tableau[m][j] = -z[j]; //Tranformando em negativo para PADRONIZAR
-            }
+            //Variáveis de folga
+            tableau[i][n + i] = 1;
 
-            //Folgas da função objetivo = 0
-            for(int j = 0; j < m; j++){
-                tableau[m][n + j] = 0;
-            }
+            //Coluna b
+            tableau[i][n + m] = b[i];
+        }
 
-            //b da função objetivo = 0
-            tableau[m][n + m] = 0;
-        //}
+        //Função objetivo
+        for (int j = 0; j < n; j++) {
+            tableau[m][j] = -z[j]; //Tranformando em negativo para PADRONIZAR
+        }
 
-        //Mostando a Primeira Tabela 
+        //Mostrando a Primeira Tabela
         System.out.println("Tabela Inicial: ");
         imprimirTableu(tableau, var, n, m);
 
@@ -100,23 +86,22 @@ public class Simplex {
         resolverTableu(tableau, var, n, m);
     }
 
-    public static void resolverTableu(double[][] tableau, String[] var, int n, int m){
-
-        while(true){
+    public static void resolverTableu(double[][] tableau, String[] var, int n, int m) {
+        while (true) {
             int colunaPivo = 0;
             double menor = tableau[m][0];
 
             //Coluna do Menor Pivo
-            for(int j = 1; j < n + m; j++){
-                if(tableau[m][j] < menor){
-                    menor = tableau[m][j]; //Pegando o menor valor da linha de Z
+            for (int j = 1; j < n + m; j++) {
+                if (tableau[m][j] < menor) {
+                    menor = tableau[m][j];
                     colunaPivo = j;
                 }
             }
 
             //Se não existir número negativo no Z:
             //solução ótima encontrada
-            if(menor >= 0){
+            if (menor >= 0){
                 break;
             }
 
@@ -124,12 +109,12 @@ public class Simplex {
             int linhaPivo = -1;
             double menorRazao = Double.MAX_VALUE;
 
-            for(int i = 0; i < m; i++){
+            for (int i = 0; i < m; i++) {
                 double elemento = tableau[i][colunaPivo];
                 //evita divisão por zero ou negativos
-                if(elemento > 0){
-                    double razao = tableau[i][n + m] / elemento; //razao = b / pelo valor na coluna do pivo
-                    if(razao < menorRazao){
+                if (elemento > 0) {
+                    double razao = tableau[i][n + m] / elemento;
+                    if (razao < menorRazao) {
                         menorRazao = razao;
                         linhaPivo = i;
                     }
@@ -137,7 +122,7 @@ public class Simplex {
             }
 
             //problema ilimitado
-            if(linhaPivo == -1){
+            if (linhaPivo == -1) {
                 System.out.println("Solução ilimitada.");
                 return;
             }
@@ -151,16 +136,16 @@ public class Simplex {
 
             //Normalizando a linha do pivo
             double pivo = tableau[linhaPivo][colunaPivo];
-
-            for(int j = 0; j < n + m + 1; j++){
-                tableau[linhaPivo][j] /= pivo; 
+            for (int j = 0; j < n + m + 1; j++) {
+                tableau[linhaPivo][j] /= pivo;
             }
 
             //Zerando coluna do pivo
-            for(int i = 0; i < m + 1; i++){
-                if(i != linhaPivo){
+            for (int i = 0; i <= m; i++) {
+                if (i != linhaPivo) {
                     double fator = tableau[i][colunaPivo];
-                    for(int j = 0; j < n + m + 1; j++){
+
+                    for (int j = 0; j < n + m + 1; j++) {
                         tableau[i][j] -= fator * tableau[linhaPivo][j];
                     }
                 }
@@ -175,45 +160,48 @@ public class Simplex {
 
         //Solução ótima
         System.out.println("\nSolução ótima encontrada:");
-        for(int i = 0; i < m; i++){
+        for (int i = 0; i < m; i++) {
             System.out.println(var[i] + " = " + tableau[i][n + m]);
         }
+
         System.out.println("Z = " + tableau[m][n + m]);
     }
-   
-    public static void imprimirTableu(double[][] tableau, String[] var, int n, int m){
+
+    public static void imprimirTableu(double[][] tableau, String[] var, int n, int m) {
+
         System.out.printf("| %-18s ", "Variaveis Basicas");
 
         //Variaveis
-        for(int i = 0; i < n; i++){
-            System.out.printf("| %-8s ", "x"+(i+1));
+        for (int i = 0; i < n; i++) {
+            System.out.printf("| %-8s ", "x" + (i + 1));
         }
 
         //Variaveis de folga
-        for(int i = 0; i < m; i++){
-            System.out.printf("| %-8s ", "f"+(i+1));
+        for (int i = 0; i < m; i++) {
+            System.out.printf("| %-8s ", "f" + (i + 1));
         }
 
         //Coluna de valores das restricoes
         System.out.printf("| %-8s |\n", "b");
 
         //Linhas das restrições
-        for(int i = 0; i < m; i++){
+        for (int i = 0; i < m; i++) {
             System.out.printf("| %-18s ", var[i]);
-            for(int j = 0; j < n + m + 1; j++){
+
+            for (int j = 0; j < n + m + 1; j++) {
                 System.out.printf("| %-8.2f ", tableau[i][j]);
             }
+
             System.out.println("|");
         }
 
         //Linha da função objetivo
         System.out.printf("| %-18s ", "z");
 
-        for(int j = 0; j < n + m + 1; j++){
+        for (int j = 0; j < n + m + 1; j++) {
             System.out.printf("| %-8.2f ", tableau[m][j]);
         }
 
         System.out.println("|");
     }
 }
-
--- src/SimplexDuasFases.java
+++ src/SimplexDuasFases.java
@@ -3,13 +3,12 @@ import java.util.Scanner;
 public class SimplexDuasFases {
 
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
 
         //Variáveis para -> x1, x2, ..., xn
         System.out.print("Informe o Número de Variáveis: ");
         int n = sc.nextInt();
-        
+
         //Quantidade de Restricoes
         System.out.print("Informe o Número de Restrições: ");
         int m = sc.nextInt();
@@ -33,169 +32,387 @@ public class SimplexDuasFases {
         int qtdArtificial = 0;
 
         //Montando a Função Objetivo (z)
-        for(int i = 0; i < n; i++){
-            System.out.print("Informe o coeficiente x"+(i+1)+": ");
+        for (int i = 0; i < n; i++) {
+            System.out.print("Informe o coeficiente x" + (i + 1) + ": ");
             z[i] = sc.nextDouble();
         }
 
         System.out.print("Função objetivo: z = ");
-        for(int i = 0; i < n; i++){
-            if(i == n-1){
-                System.out.print("("+z[i]+")x"+(i+1));
-            }else{
-                System.out.print("("+z[i]+")x"+(i+1)+" + ");    
-            } 
+        for (int i = 0; i < n; i++) {
+            if (i == n - 1) {
+                System.out.print("(" + z[i] + ")x" + (i + 1));
+            } else {
+                System.out.print("(" + z[i] + ")x" + (i + 1) + " + ");
+            }
         }
         System.out.println();
 
         //Montando as Restricoes
-        for(int i = 0; i < m; i++){
-            System.out.println("\nRestrição "+(i+1));
+        for (int i = 0; i < m; i++) {
+            System.out.println("\nRestrição " + (i + 1));
 
-            for(int j = 0; j < n; j++){
-                System.out.print("Informe o coeficiente x"+(j+1)+": ");
+            for (int j = 0; j < n; j++) {
+                System.out.print("Informe o coeficiente x" + (j + 1) + ": ");
                 a[i][j] = sc.nextDouble();
             }
 
             System.out.print("Tipo (<=, >=, =): ");
             tipo[i] = sc.next();
+
             System.out.print("Resultado (b): ");
             b[i] = sc.nextDouble();
 
             //contagem da quantidade de variaveis auxiliares
-            if(tipo[i].equals("<=")){
+            if (tipo[i].equals("<=")) {
                 qtdFolga++;
-            }else if(tipo[i].equals(">=")){
+
+            } else if (tipo[i].equals(">=")) {
                 qtdExcesso++;
                 qtdArtificial++;
-            }else{
+
+            } else {
                 qtdArtificial++;
             }
         }
-
-        //Matriz das folga, excesso e artificial
-        double[][] folga = new double[m][qtdFolga];
-
-        double[][] excesso = new double[m][qtdExcesso];
-
-        double[][] artificial = new double[m][qtdArtificial];
 
         //variavel basica
         String[] var = new String[m];
 
-        //preenchendo matrizes
+        //Tamanho da tabela
+        int totalColunas = n + qtdFolga + qtdExcesso + qtdArtificial + 1;
+
+        //Tableau (+1 linha para W)
+        double[][] tableau = new double[m + 2][totalColunas];
+
+        int colFolga = n;
+        int colExcesso = n + qtdFolga;
+        int colArtificial = n + qtdFolga + qtdExcesso;
+
         int f = 0;
         int e = 0;
         int art = 0;
 
-        for(int i = 0; i < m; i++){
-            if(tipo[i].equals("<=")){
-                folga[i][f] = 1;
+        //Montando o Tableu
+        //Preenchendo a tabela com as Restricoes
+        for (int i = 0; i < m; i++) {
+            //variáveis normais
+            for (int j = 0; j < n; j++) {
+                tableau[i][j] = a[i][j];
+            }
+
+            //folga
+            if (tipo[i].equals("<=")) {
+                tableau[i][colFolga + f] = 1;
                 var[i] = "f" + (f + 1);
                 f++;
+            }
 
-            }else if(tipo[i].equals(">=")){
-                excesso[i][e] = -1;
-                artificial[i][art] = 1;
+            //excesso e artificial
+            else if (tipo[i].equals(">=")) {
+                tableau[i][colExcesso + e] = -1;
+                tableau[i][colArtificial + art] = 1;
+
                 var[i] = "a" + (art + 1);
+
                 e++;
                 art++;
+            }
 
-            }else{
-                artificial[i][art] = 1;
+            //apenas artificial
+            else {
+                tableau[i][colArtificial + art] = 1;
                 var[i] = "a" + (art + 1);
                 art++;
             }
+
+            //resultado
+            tableau[i][totalColunas - 1] = b[i];
         }
 
-        //Tamanho da tabela
-        int totalColunas = n + qtdFolga + qtdExcesso + qtdArtificial + 1;
-
-        //Tableau
-        double[][] tableau = new double[m + 1][totalColunas];
-
-        //Montando o Tableu{
-            //Preenchendo a tabela com as Restricoes
-            for(int i = 0; i < m; i++){
-                //variáveis normais
-                for(int j = 0; j < n; j++){
-                    tableau[i][j] = a[i][j];
-                }
-
-                //folga
-                for(int j = 0; j < qtdFolga; j++){
-                    tableau[i][n + j] = folga[i][j];
-                }
-
-                //excesso
-                for(int j = 0; j < qtdExcesso; j++){
-                    tableau[i][n + qtdFolga + j] = excesso[i][j];
-                }
-
-                //artificial
-                for(int j = 0; j < qtdArtificial; j++){
-                    tableau[i][n + qtdFolga + qtdExcesso + j] = artificial[i][j];
-                }
-
-                //resultado
-                tableau[i][totalColunas - 1] = b[i];
-            }
-        //}
-
         //Funcao Objetivo
-        for(int j = 0; j < n; j++){
+        for (int j = 0; j < n; j++) {
             tableau[m][j] = -z[j];
+        }
+
+        //Função W
+        for (int j = 0; j < qtdArtificial; j++) {
+            tableau[m + 1][colArtificial + j] = -1;
+        }
+
+        //Como as variáveis artificiais já estão na base, precisamos zerar seus coeficientes na linha W
+        for (int i = 0; i < m; i++) {
+            if (var[i].startsWith("a")) {
+                for (int j = 0; j < totalColunas; j++) {
+                    tableau[m + 1][j] += tableau[i][j];
+                }
+            }
         }
 
         //Mostrando a Primeira Tabela
         System.out.println("Tabela Inicial: ");
         imprimirTableau(tableau, var, n, qtdFolga, qtdExcesso, qtdArtificial);
+
+        //Resultado Final
+        resolverTableu(tableau, var, n, qtdFolga, qtdExcesso, qtdArtificial);
     }
 
-    public static void imprimirTableau(double[][] tableau, String[] var, int n, int qtdFolga, int qtdExcesso, int qtdArtificial){
+    public static void resolverTableu(double[][] tableau, String[] var, int n, int qtdFolga, int qtdExcesso, int qtdArtificial) {
+        int m = tableau.length - 2;
+        int totalColunas = tableau[0].length;
+
+        //FASE 1
+        if (qtdArtificial > 0) {
+            //Loop de iterações da Fase 1
+            while (true) {
+
+                int colunaPivo = -1;
+                double maiorW = 0;
+
+                //Buscando maior valor positivo da linha W
+                for (int j = 0; j < totalColunas - 1; j++) {
+                    if (tableau[m + 1][j] > maiorW) {
+                        maiorW = tableau[m + 1][j];
+                        colunaPivo = j;
+                    }
+                }
+
+                //Se não houver nenhum valor positivo maior que zero, a Fase 1 terminou
+                if (colunaPivo == -1 || maiorW <= 1e-9) {
+                    break;
+                }
+
+                //Linha do menor Pivo
+                int linhaPivo = -1;
+                double menorRazao = Double.MAX_VALUE;
+
+                for (int i = 0; i < m; i++) {
+                    double elemento = tableau[i][colunaPivo];
+
+                    //evita divisão por zero ou negativos
+                    if (elemento > 1e-9) {
+                        double razao = tableau[i][totalColunas - 1] / elemento;
+                        if (razao < menorRazao) {
+                            menorRazao = razao;
+                            linhaPivo = i;
+                        }
+                    }
+                }
+
+                //problema ilimitado
+                if (linhaPivo == -1) {
+                    System.out.println("Solução ilimitada.");
+                    return;
+                }
+
+                //Muda a variável básica
+                atualizarVariavelBasica(var, linhaPivo, colunaPivo, n, qtdFolga, qtdExcesso);
+
+                //Normalizando a linha do pivo
+                double pivo = tableau[linhaPivo][colunaPivo];
+
+                for (int j = 0; j < totalColunas; j++) {
+                    tableau[linhaPivo][j] /= pivo;
+                }
+
+                //Zerando coluna do pivo
+                for (int i = 0; i < tableau.length; i++) {
+                    if (i != linhaPivo) {
+                        double fator = tableau[i][colunaPivo];
+                        for (int j = 0; j < totalColunas; j++) {
+                            tableau[i][j] -= fator * tableau[linhaPivo][j];
+                        }
+                    }
+                }
+            }
+
+            //Verifica se W é igual a zero
+            if (Math.abs(tableau[m + 1][totalColunas - 1]) > 1e-5) {
+                System.out.println("O problema não possui solução viável (W final != 0).");
+                return;
+            }
+
+            //Zerando coluna das variáveis artificiais
+            int inicioArtificiais = n + qtdFolga + qtdExcesso;
+            for (int i = 0; i < tableau.length; i++) {
+                for (int j = inicioArtificiais; j < inicioArtificiais + qtdArtificial; j++) {
+                    tableau[i][j] = 0;
+                }
+            }
+        }
+
+        //FASE 2
+        //Como a base mudou na Fase 1, precisamos garantir que as variáveis básicas atuais estejam zeradas na linha de Z
+        for (int i = 0; i < m; i++) {
+            String vBasica = var[i];
+            int colunaBasica = -1;
+
+            //Descobre o índice da coluna da variável básica atual
+            if (vBasica.startsWith("x")) {
+                colunaBasica = Integer.parseInt(vBasica.substring(1)) - 1;
+            } else if (vBasica.startsWith("f")) {
+                colunaBasica = n + Integer.parseInt(vBasica.substring(1)) - 1;
+            } else if (vBasica.startsWith("e")) {
+                colunaBasica = n + qtdFolga + Integer.parseInt(vBasica.substring(1)) - 1;
+            }
+            if (colunaBasica != -1 && Math.abs(tableau[m][colunaBasica]) > 1e-9) {
+                double fator = tableau[m][colunaBasica];
+                for (int j = 0; j < totalColunas; j++) {
+                    tableau[m][j] -= fator * tableau[i][j];
+                }
+            }
+        }
+
+        //Execução do Simplex 
+        while (true) {
+
+            int colunaPivo = 0;
+            double menor = tableau[m][0];
+
+            //Procura o menor valor negativo na linha Z
+            int limite = n + qtdFolga + qtdExcesso;
+
+            for (int j = 1; j < limite; j++) {
+                if (tableau[m][j] < menor) {
+                    menor = tableau[m][j];
+                    colunaPivo = j;
+                }
+            }
+
+            //Critério de parada
+            if (menor >= -1e-9) {
+                break;
+            }
+
+            //Encontra a linha pivô pela menor razão
+            int linhaPivo = -1;
+            double menorRazao = Double.MAX_VALUE;
+
+            for (int i = 0; i < m; i++) {
+                double elemento = tableau[i][colunaPivo];
+                if (elemento > 1e-9) {
+                    double razao = tableau[i][totalColunas - 1] / elemento;
+                    if (razao < menorRazao) {
+                        menorRazao = razao;
+                        linhaPivo = i;
+                    }
+                }
+            }
+
+            //problema ilimitado
+            if (linhaPivo == -1) {
+                System.out.println("Solução ilimitada detectada na Fase 2.");
+                return;
+            }
+
+            //Troca a variável da base
+            atualizarVariavelBasica(var, linhaPivo, colunaPivo, n, qtdFolga, qtdExcesso);
+
+            //Normalização da linha pivô
+            double pivo = tableau[linhaPivo][colunaPivo];
+
+            for (int j = 0; j < totalColunas; j++) {
+                tableau[linhaPivo][j] /= pivo;
+            }
+
+            //Eliminação de Gauss-Jordan para zerar a coluna pivô
+            for (int i = 0; i < m + 1; i++) {
+                if (i != linhaPivo) {
+                    double fator = tableau[i][colunaPivo];
+                    for (int j = 0; j < totalColunas; j++) {
+                        tableau[i][j] -= fator * tableau[linhaPivo][j];
+                    }
+                }
+            }
+        }
+
+        //Tabela Final
+        System.out.println("Tabela Final:");
+        imprimirTableau(tableau, var, n, qtdFolga, qtdExcesso, qtdArtificial);
+        System.out.println();
+
+        //Solução Ótima
+        System.out.println("Solução ótima encontrada:");
+
+        //Variáveis de decisão (x1, x2, ..., xn)
+        double[] valoresX = new double[n];
+
+        for(int i = 0; i < m; i++){
+            if(var[i].startsWith("x")){
+                int indice = Integer.parseInt(var[i].substring(1)) - 1;
+                valoresX[indice] = tableau[i][totalColunas - 1];
+            }
+        }
+
+        //Mostra todas as variáveis de decisão
+        for(int i = 0; i < n; i++){
+            System.out.printf("x%d = %.2f\n", i + 1, valoresX[i]);
+        }
+
+        System.out.printf("Z = %.2f\n", tableau[m][totalColunas - 1]);
+    }
+
+    //Método para muda a variavel básica na tabela final
+    private static void atualizarVariavelBasica(String[] var, int linhaPivo, int colunaPivo, int n, int qtdFolga, int qtdExcesso) {
+        if (colunaPivo < n) {
+            var[linhaPivo] = "x" + (colunaPivo + 1);
+        } else if (colunaPivo < n + qtdFolga) {
+            var[linhaPivo] = "f" + (colunaPivo - n + 1);
+        } else if (colunaPivo < n + qtdFolga + qtdExcesso) {
+            var[linhaPivo] = "e" + (colunaPivo - n - qtdFolga + 1);
+        } else {
+            var[linhaPivo] = "a" + (colunaPivo - n - qtdFolga - qtdExcesso + 1);
+        }
+    }
+
+    public static void imprimirTableau(double[][] tableau, String[] var, int n, int qtdFolga, int qtdExcesso, int qtdArtificial) {
         System.out.printf("| %-18s ", "Variaveis Basicas");
 
         //Variaveis
-        for(int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             System.out.printf("| %-8s ", "x" + (i + 1));
         }
 
         //Variaveis de folga
-        for(int i = 0; i < qtdFolga; i++){
+        for (int i = 0; i < qtdFolga; i++) {
             System.out.printf("| %-8s ", "f" + (i + 1));
         }
 
         //Variaveis de excesso
-        for(int i = 0; i < qtdExcesso; i++){
+        for (int i = 0; i < qtdExcesso; i++) {
             System.out.printf("| %-8s ", "e" + (i + 1));
         }
 
         //Variaveis artificial
-        for(int i = 0; i < qtdArtificial; i++){
+        for (int i = 0; i < qtdArtificial; i++) {
             System.out.printf("| %-8s ", "a" + (i + 1));
         }
 
         //Coluna de valores das restricoes
         System.out.printf("| %-8s |\n", "b");
-
-        int m = tableau.length - 1;
+        int m = tableau.length - 2;
 
         //Linhas das restrições
-        for(int i = 0; i < m; i++){
+        for (int i = 0; i < m; i++) {
             System.out.printf("| %-18s ", var[i]);
-            for(int j = 0; j < tableau[0].length; j++){
+            for (int j = 0; j < tableau[0].length; j++) {
                 System.out.printf("| %-8.2f ", tableau[i][j]);
             }
             System.out.println("|");
         }
 
+        //Linha da função W
+        System.out.printf("| %-18s ", "W");
+        for (int j = 0; j < tableau[0].length; j++) {
+            System.out.printf("| %-8.2f ", tableau[m + 1][j]);
+        }
+        System.out.println("|");
+
+
         //Linha da função objetivo
         System.out.printf("| %-18s ", "Z");
-
-        for(int j = 0; j < tableau[0].length; j++){
+        for (int j = 0; j < tableau[0].length; j++) {
             System.out.printf("| %-8.2f ", tableau[m][j]);
         }
-
         System.out.println("|");
     }
 }
